d396ccd8987d695412cf6aebccf24ffb6c13e856
Jeongpyo <jp.941009@gmail.com>
2023-07-19 02:48:14 +0900

    blog last commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 
 /* eslint-disable */
 
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -35,12 +35,33 @@ function App() {
     setTitle(arr);
   };
 
+  // input
+  const [input, setInput] = useState("");
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  // 게시글 추가
+  const addTitle = () => {
+    const copy = [...title];
+    copy.unshift(input);
+    setTitle(copy);
+    setLikeCnt([...likeCnt, 0]);
+    setInput("");
+  };
+
+  // 게시글 삭제
+  const removeTitle = (e, idx) => {
+    e.stopPropagation();
+    setTitle(title.filter((_, i) => i !== idx));
+  };
+
   // Modal창 상태
-  const [modal, setModal] = useState([false, false, false]);
+  const [show, setShow] = useState(false);
+  const [titleIdx, setTitleIdx] = useState(0);
   const modalToggle = (idx) => {
-    const copy = [...modal];
-    copy[idx] = true;
-    setModal(copy);
+    setTitleIdx(idx);
+    setShow(!show);
   };
 
   return (
@@ -50,7 +71,6 @@ function App() {
       </div>
       <button onClick={changeTitle}>제목 변경</button>
       <button onClick={orderByAsc}>가나다 순으로 정렬</button>
-
       {title.map((title, idx) => (
         <div className="list" key={idx}>
           <h4 onClick={() => modalToggle(idx)}>
@@ -58,25 +78,58 @@ function App() {
             {likeCnt[idx]}
           </h4>
           <p>2월 17일 발행</p>
+          <button onClick={(e) => removeTitle(e, idx)}>삭제</button>
         </div>
       ))}
 
+      {/* input */}
+      <input onChange={onChange} type="text" />
+      <button onClick={addTitle}>등록</button>
+
       {/* Modal */}
-      {title.map((title, idx) => (
-        <Modal title={title} key={idx} isShow={modal[idx]} />
-      ))}
+      <Modal
+        title={title}
+        titleIdx={titleIdx}
+        changeTitle={changeTitle}
+        show={show}
+      />
+      <Modal2 />
     </div>
   );
 }
 
-const Modal = ({ title, isShow }) => {
-  return isShow ? (
+const Modal = ({ title, titleIdx, changeTitle, show }) => {
+  return show ? (
     <div className="modal">
-      <h4>{title}</h4>
+      <h4>{title[titleIdx]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={changeTitle}>글수정</button>
     </div>
   ) : null;
 };
+
+/**
+ * class 방식으로 컴포넌트 만드는 방법
+ * constructor, super, render 세 가지부터 써놓고 시작
+ */
+class Modal2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "hong",
+      age: 28,
+    };
+  }
+  render() {
+    return (
+      <div>
+        안녕 {this.state.name}
+        {this.state.age}
+        <button onClick={() => this.setState({ age: 30 })}>버튼</button>
+      </div>
+    );
+  }
+}
 
 export default App;
